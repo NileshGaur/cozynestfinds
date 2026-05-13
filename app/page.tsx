50bf3cfe593f00a82c900895d0a36b9901ba0e90
Nilesh <nileshgaur99@gmail.com>
2026-05-14 00:55:54 +0530

    build homepage product pages and admin

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ export default async function Home() {
         </div>
 
         <div className="mt-8 grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-          {products.map((product) => (
+          {products?.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
